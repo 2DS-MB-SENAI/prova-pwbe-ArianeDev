7f09c598f73adbaadaa719a1cf4d06e475e8f14b
Ariane Silva <silvaarianeoliveira2@gmail.com>
2025-04-04 10:16:26 -0300

    .

--- agenda/urls.py
+++ agenda/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('servicos/', views.servicos, name="Serviços"),
     path('servicos/<int:pk>/', views.get_servico, name="Serviços"),
-    path('agendamento', views.agendamentos, name="Agendamento"),
+    path('agendamentos/', views.agendamentos, name="Agendamento"),
     path('agendamento/<int:pk>', views.get_agendamento, name="Agendamento"),
 ]
