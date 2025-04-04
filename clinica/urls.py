ed3d2a48862b371e229a1522a42b6ab8c58f63ac
Ariane Silva <silvaarianeoliveira2@gmail.com>
2025-04-04 11:36:16 -0300

    Task 3

--- clinica/urls.py
+++ clinica/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('medicos/', views.get_medicos, name="listar_medicos"),
-    path('consultas/nova', views.post_consulta, name="criar_consulta"),
+    path('consultas/nova', views.post_consulta, name="form_consulta"),
 ]
--- custom_auth/urls.py
+++ custom_auth/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('register/', views.post_cadastrar, name="Cadastrar"),
     path('login/', views.login, name="Logar"),
+    path('profile/', views.get_profile),
+    path('profile/', views.patch_profile),
 ]
